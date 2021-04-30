83c80ae4a26064659abdce64b1647b798ea90bd0
sashikala <sashikala_sn@yahoo.com>
2021-04-30 15:07:02 -0400

    Fixing the compile issue

--- src/main/java/com/fsiautism_4/springboot/dao/DBDataSource.java
+++ src/main/java/com/fsiautism_4/springboot/dao/DBDataSource.java
@@ -1,7 +1,6 @@
 package com.fsiautism_4.springboot.dao;
 
 import java.io.IOException;
-
 import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
@@ -9,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.fsiautism_4.springboot.Application;
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
 
 @Configuration
 public class DBDataSource {
@@ -19,12 +18,12 @@ public class DBDataSource {
 		Properties properties = new Properties();
 		properties.load(Application.class.getClassLoader().getResourceAsStream("application.properties"));
 
-		SQLServerDataSource dataSource = new SQLServerDataSource();
-
-		dataSource.setUser(properties.get("user").toString());
-		dataSource.setPassword(properties.get("password").toString());
-		dataSource.setURL(properties.get("url").toString());
-		return new JdbcTemplate(dataSource);
+//		SQLServerDataSource dataSource = new SQLServerDataSource();
+//
+//		dataSource.setUser(properties.get("user").toString());
+//		dataSource.setPassword(properties.get("password").toString());
+//		dataSource.setURL(properties.get("url").toString());
+		return null;
 	}
 
 }
